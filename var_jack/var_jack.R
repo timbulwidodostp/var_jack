c5264f62ff6db1f8ce0aa030ad2e20ebd4e6dd88
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-03-31 17:27:30 +0700

    Use var.jack (pls) With (In) R Software

--- var_jack/var_jack.R
+++ var_jack/var_jack.R
@@ -1,17 +1,17 @@
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
-# Jackknife variance estimates of regression coefficients Use var_jack (pls) With (In) R Software
+# Jackknife variance estimates of regression coefficients Use var.jack (pls) With (In) R Software
 install.packages("pls")
 library("pls")
 var_jack = read.csv("https://raw.githubusercontent.com/timbulwidodostp/var_jack/main/var_jack/var_jack.csv",sep = ";")
-# Estimation Jackknife variance estimates of regression coefficients Use var_jack (pls) With (In) R Software
+# Estimation Jackknife variance estimates of regression coefficients Use var.jack (pls) With (In) R Software
 Y <- cbind(var_jack$Y_1, var_jack$Y_2, var_jack$Y_3, var_jack$Y_4, var_jack$Y_5, var_jack$Y_6)
 X <- cbind(var_jack$X_1, var_jack$X_2, var_jack$X_3, var_jack$X_4, var_jack$X_5)
 var_jack <- pcr(Y ~ X, data = var_jack, validation = "LOO", jackknife = TRUE)
 var.jack <- var.jack(var_jack, ncomp = 2)
 var.jack
-# Jackknife variance estimates of regression coefficients Use var_jack (pls) With (In) R Software
+# Jackknife variance estimates of regression coefficients Use var.jack (pls) With (In) R Software
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
